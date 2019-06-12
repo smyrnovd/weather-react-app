6ee1049a417ff4d134f4be8e86bcb4ffb8168435
Dmitry Smyrnov <smyrnovd@gmail.com>
2019-06-13 01:30:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Info from './components/info';
+import Form from './components/form';
+import Weather from './components/weather';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Info />
+                <Form />
+                <Weather />
+            </div>
+        );
+    }
+}
+
+export default App;
