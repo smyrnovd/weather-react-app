cb22626a9b2343a07862a212e1394a40bad39078
Dmitry Smyrnov <smyrnovd@gmail.com>
2019-06-13 13:05:37 +0300

    getting weather form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Info from './components/info';
 import Form from './components/form';
 import Weather from './components/weather';
+import { async } from 'q';
 
+const API_KEY = "9cc13cad091c7b137e9f92303ce37b72";
+ 
 class App extends React.Component{
+   
+    gettingWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city }&appid=${API_KEY}`);
+        const data = await api_url.json();
+        console.log(data);
+    }
     render(){
         return(
             <div>
                 <Info />
-                <Form />
+                <Form weatherMethod={this.gettingWeather} />
                 <Weather />
             </div>
         );
--- src/components/form.js
+++ src/components/form.js
@@ -3,9 +3,10 @@ import React from 'react';
 class Form extends React.Component{
     render(){
         return(
-            <div>
-                <p>Компонент формы</p>
-            </div>
+           <form onSubmit={this.props.weatherMethod}>
+               <input type="text" name="city" placeholder="City"/>
+               <button>Get weather</button>
+           </form>
         );
     }
 }
